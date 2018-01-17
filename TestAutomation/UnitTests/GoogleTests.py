0c5b2df79dbefa54905b656d444f7a30099c8425
jmshapland <jmshapland@gmail.com>
2018-01-16 21:46:56 -0600

    Updated solution to work with python 3.6.2.

--- TestAutomation/UnitTests/GoogleTests.py
+++ TestAutomation/UnitTests/GoogleTests.py
@@ -13,40 +13,40 @@ class GoogleTestCase(unittest.TestCase):
     # preparing to test
     def setUp(self):
         """ Setting up for the test """
-        print "GoogleTestCase:setUp_:begin"
+        print("GoogleTestCase:setUp_:begin")
 
         # Optional argument, if not specified will search path.
         # TODO: UPDATE THIS PATH TO REFLECT WHERE YOU HAVE YOUR SOURCE CODE STORED.
         #       WE SHOULD UPDATE THIS LATER TO A CLEANER METHOD.
-        self.browser = webdriver.Chrome('C:\git\OFD\TestAutomation\Drivers\chromedriver.exe')
+        self.browser = webdriver.Chrome('C:\git\OFD-Test-Automation\TestAutomation\Drivers\chromedriver.exe')
 
         self.addCleanup(self.browser.quit)
         name = self.shortDescription()
-        print "setting up for " + name
+        print("setting up for " + name)
 
-        print "GoogleTestCase:setUp_:end"
+        print("GoogleTestCase:setUp_:end")
 
     # ending the test
     def tearDown(self):
         """Cleaning up after the test"""
-        print "GoogleTestCase:tearDown_:begin"
+        print("GoogleTestCase:tearDown_:begin")
 
         name = self.shortDescription()
-        print "cleaning up after " + name
+        print("cleaning up after " + name)
 
-        print "GoogleTestCase:tearDown_:end"
+        print("GoogleTestCase:tearDown_:end")
 
     # test that Google's page title says 'Google'
     def testGooglePageTitle(self):
         """Test routine: testGooglePageTitle"""
-        print "GoogleTestCase:testGooglePageTitle"
+        print ("GoogleTestCase:testGooglePageTitle")
         self.browser.get('http://www.google.com')
         self.assertIn('Google', self.browser.title)
 
     #test Google's search functionality by searching for selenium
     def testSearch(self):
         """Test routine: testGoogleSearch"""
-        print "GoogleTestCase:testGoogleSearch"
+        print("GoogleTestCase:testGoogleSearch")
         self.browser.get('http://www.google.com')
         elem = self.browser.find_element_by_id('lst-ib')
         elem.send_keys('seleniumhq' + Keys.RETURN)
@@ -59,23 +59,23 @@ if __name__ == '__main__':
     googleRunner = unittest.TextTestRunner()
     googleResult = googleRunner.run(googleSuite)
 
-    print
-    print "---- START OF TEST RESULTS"
-    print googleResult
-    print
-    print "fooResult::errors"
-    print googleResult.errors
-    print
-    print "fooResult::failures"
-    print googleResult.failures
-    print
-    print "fooResult::skipped"
-    print googleResult.skipped
-    print
-    print "fooResult::successful"
-    print googleResult.wasSuccessful()
-    print
-    print "fooResult::test-run"
-    print googleResult.testsRun
-    print "---- END OF TEST RESULTS"
-    print
+    print()
+    print("---- START OF TEST RESULTS")
+    print(googleResult)
+    print()
+    print("fooResult::errors")
+    print(googleResult.errors)
+    print()
+    print("fooResult::failures")
+    print(googleResult.failures)
+    print()
+    print("fooResult::skipped")
+    print(googleResult.skipped)
+    print()
+    print("fooResult::successful")
+    print(googleResult.wasSuccessful())
+    print()
+    print("fooResult::test-run")
+    print(googleResult.testsRun)
+    print("---- END OF TEST RESULTS")
+    print()
